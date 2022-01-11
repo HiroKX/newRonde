5ab776a24a4cf24ac1e2346edf33d17384d8325b
RICHARD Eddy <richard.eddy@live.fr>
2022-01-11 21:25:08 +0100

    fixture: setting slick carrousel

--- assets/carousel.js
+++ assets/carousel.js
@@ -4,6 +4,10 @@ import 'slick-carousel/slick/slick';
 const $ = require('jquery');
 global.$ = global.jQuery = $;
 
-$('.image-carousel').slick();
+$('.image-carousel').slick({
+    infinite: true,
+    dots: true,
+    speed: 500
+});
 
 console.debug('carousel.js loaded');
