b63b7b9bbd1ed050be697213a46ff27d627ee0dd
RICHARD Eddy <richard.eddy@live.fr>
2022-01-11 15:24:31 +0100

    fixture: Upload images

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -47,7 +47,7 @@ class Article
     #[JoinTable(name: "article_image_attachment")]
     #[JoinColumn(name: "article_id", referencedColumnName: "id")]
     #[InverseJoinColumn(name: "attachments_id", referencedColumnName: "id")]
-    private ?Collection $images = null;
+    private Collection $images;
 
     #[ORM\Column(type: 'datetime')]
     private \DateTimeInterface $dateAdd;
@@ -56,6 +56,7 @@ class Article
     {
         $this->dateAdd = new \DateTime();
         $this->attachments = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     /**
